1f4a505d5d4b0fd6fa54bfc2c5e71fcf7bfc92fc
Yuri Moraes Lira Mota <yuri.mota@outlook.com.br>
2022-07-25 13:22:37 -0300

    Inicial

---
+++ src/test/java/com/mota/app/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mota.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
